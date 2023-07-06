f8bdb8c8de0bbb95ac7250ad90759bf05314e969
Your Name <shivaadhikari0214@gmail.com>
2023-07-07 02:40:05 +0300

    First commit

---
+++ trappingRainwater.java
@@ -0,0 +1,11 @@
+public class trappingRainwater{
+
+    /* Conditions needed. */
+    // 1. To fill a bar the minimum number of bars must be greater than 2. (MIN. NO. OF BARS>2).
+    // 2. If bars are in ascending and descending order. NO water will be trapped.
+    // 3. Left and right maximum boundary should be checked. And the minimum should be taken from both of the boundary. For eg. IN 6,2,8 capacity to hold water will be. (6-2)*width.
+    //But in case of 10,6,2,8 minimum will be 8. So conditions changes here.
+    public static void main(String args[]){
+
+    }
+}
