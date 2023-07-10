7cdf1e124ce8b4e735a5218c49380f052d82db31
Your Name <shivaadhikari0214@gmail.com>
2023-07-10 19:55:23 +0300

    New changes

---
+++ bubbleSort.java
@@ -0,0 +1,48 @@
+public class bubbleSort {
+    // public static void getBubbleSorted(int arr[]){
+        
+    //     for(int turn=0;turn<=arr.length-1;turn++){
+    //         for(int i=0;i<arr.length-1-turn;i++){
+    //             if(arr[i]>arr[i+1]){
+    //                 //swap
+    //                 int temp = arr[i];
+    //                 arr[i] = arr[i+1];
+    //                 arr[i+1] = temp;
+    //             } 
+    //         }
+    //     }
+    // }
+
+
+    public static void printSorted(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    // In selection sorted we don't swap from the beginning. First we find out the smallest in the whole array 
+    //place it accordingly to the index. And we swap the other elements respectively.
+    public static void getSelectionSorted(int arr[]){
+        for(int i=0;i<arr.length-2;i++){
+            int minPos = i ;
+            for(int j=i+1;j<arr.length-1;j++){
+                if(arr[minPos]>arr[j]){
+                    minPos = j;
+                   
+                }
+            }
+            // swap the lowest number in the j place.
+            int temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+
+        }
+
+    }
+    public static void main(String[] args){
+        int arr[] = {5,6,3,2,7};
+        getSelectionSorted(arr);
+        printSorted(arr);
+    }
+}
