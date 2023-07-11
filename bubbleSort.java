b0afc1ed6c99626a7df5ef429a3ad2a9044fc622
Your Name <shivaadhikari0214@gmail.com>
2023-07-11 14:33:50 +0300

    Reverse order using collection reverse order

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class bubbleSort {
     // public static void getBubbleSorted(int arr[]){
@@ -16,7 +17,7 @@ public class bubbleSort {
     // }
 
 
-    public static void printSorted(int arr[]){
+    public static void printSorted(Integer arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
@@ -60,9 +61,9 @@ public class bubbleSort {
         }
     }
     public static void main(String[] args){
-        int arr[] = {5,4,1,3,2};
+        Integer arr[] = {5,4,1,3,2};
         // Array sort importing it.  Collections.reverseOrder function will help to make it in reverse order.
-        Arrays.sort(arr,0,3);
+        Arrays.sort(arr,Collections.reverseOrder());
         printSorted(arr);
     }
 }
