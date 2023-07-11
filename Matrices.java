7e5ef7b9d16470b7c4bff9272b21f72fb19b6850
Your Name <shivaadhikari0214@gmail.com>
2023-07-12 02:05:55 +0300

    sum of main diagonal and sub diagonal of matrix.

--- Matrices.java
+++ Matrices.java
@@ -25,27 +25,95 @@ public class Matrices {
         }
         return smallest_val;
     }
-    public static void main(String[] args){
-    int key = 3;
-    int matrix[][] = new int[3][4];
-    int n = 3;
-    int m = 4;
-    Scanner sc = new Scanner(System.in);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            matrix[i][j] = sc.nextInt();
-        }
-    }
+     
+    public static void printSpriral (int matrix[][]){
+        int startRow = 0;
+        int endRow = matrix.length-1;
+        int startColumn = 0;
+        int endColumn = matrix[0].length-1;
 
-    //OUTPUT
-    for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
-            System.out.print(matrix[i][j]+" ");
+        while (startColumn<=endColumn && startRow<=endRow) {
+            //top row
+            for(int j=startColumn;j<=endColumn;j++){
+                System.out.print(matrix[startRow] [j]+" ");
+            }
+            // Right column
+            for(int i=startRow+1;i<=endRow;i++){
+                System.out.print(matrix[i][endColumn]+" ");
+            }
+            //bottom boundary
+            for(int j=endColumn-1;j>=startColumn;j--){
+                if(startRow==endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j]+" ");
+            }
+            // left
+             for(int i=endRow-1;i>=startRow+1;i--){
+                if(startColumn==endColumn){
+                    break;
+                }
+                System.out.print(matrix[i][startColumn]+" ");
+            }
+            startColumn++;
+            startRow++;
+            endColumn--;
+            endRow--;
+            }
+            
         }
-        System.out.println();
+    
+    public static int sumOfDiagonal(int matrix[][]){  
+        int sum = 0;
+        for(int i=0;i<matrix.length;i++){
+            //main diagonal
+            sum += matrix[i][i];
+            // The sub diagonal of matrix.
+            if(i != matrix.length-1-i)
+                sum += matrix[i] [matrix.length-1-i];
+        }
+        return sum;
     }
-    System.out.println(Largest(matrix));
-    searchKey(matrix, key);
+    //O(n^2) Time complexity.
+    //     for(int i=0;i<matrix.length;i++){
+    //         for(int j=0;j<matrix[0].length;j++){
+    //             if(i==j){
+    //                  sum += matrix[i][j];
+    //             }
+    //             else if(i+j==matrix.length-1){
+    //                 sum += matrix[i][j];
+    //             }
+    //         }
+    //     }
+    //     return sum ;
+    // }
+    public static void main(String[] args){
+    int matrix [][] =  {{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+    System.out.println(sumOfDiagonal(matrix));
     }
 }
 
+
+ //int key = 3;
+    // int matrix[][] = new int[3][4];
+    // int n = 3;
+    // int m = 4;
+    // Scanner sc = new Scanner(System.in);
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         matrix[i][j] = sc.nextInt();
+    //     }
+    // }
+
+    // //OUTPUT
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<4;j++){
+    //         System.out.print(matrix[i][j]+" ");
+    //     }
+    //     System.out.println();
+    // }
+    // System.out.println(Largest(matrix));
+    // searchKey(matrix, key);
