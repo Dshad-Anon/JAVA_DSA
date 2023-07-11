eb612ccdc9ba4b42305b41376d1b0b2591249c74
Your Name <shivaadhikari0214@gmail.com>
2023-07-11 14:26:38 +0300

    insertion Sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -40,9 +40,26 @@ public class bubbleSort {
         }
 
     }
+
+    public static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int current = arr[i];
+            int previous = i-1;
+            //finding out the correct position to insert
+            while(previous >=0 && arr[previous]>current)
+            {
+                arr[previous+1] = arr[previous];
+                previous--;
+            }
+
+            // insertion
+            arr[previous+1] = current;
+
+        }
+    }
     public static void main(String[] args){
         int arr[] = {5,6,3,2,7};
-        getSelectionSorted(arr);
+        insertionSort(arr);
         printSorted(arr);
     }
 }
